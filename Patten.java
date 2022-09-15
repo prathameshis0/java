d0e10c524faa34887f8d9bb9cbefe5b0d2e57bd3
prathameshis0 <113047967+prathameshis0@users.noreply.github.com>
2022-09-15 18:28:06 +0530

    Add files via upload

---
+++ Patten.java
@@ -0,0 +1,24 @@
+// Id=AF0221393 Name= Prathamesh Shivgan
+import java.util.Scanner;
+class Patten
+{
+
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+for(int i=0;i<5;i++)
+{
+for(int j=i;j<5;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+
+}
+// output - *****
+            ****
+    	    ***
+	    **
+	    *
