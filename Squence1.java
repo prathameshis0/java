73aa12518ab2d7689d2b04976ff278a948f77030
prathameshis0 <113047967+prathameshis0@users.noreply.github.com>
2022-09-15 18:26:02 +0530

    Add files via upload

---
+++ Squence1.java
@@ -0,0 +1,19 @@
+// Id=AF0221393 Name= Prathamesh Shivgan
+import java.util.Scanner;
+class Squence1
+{
+
+public static void main(String[] args)
+{
+int n=1,i=3;
+System.out.print(" "+n);
+while(n<=10)
+{
+n=n+i;
+System.out.print(" "+n);
+}
+
+
+}
+}
+// 1 4 7 10 13 ...
