a43082a0aaa1cc24bc9e4f5b34a646d8af08acd8
prathameshis0 <113047967+prathameshis0@users.noreply.github.com>
2022-09-29 18:14:36 +0530

    Add files via upload

---
+++ Examplepackage.java
@@ -0,0 +1,15 @@
+// Id=AF0221393 Name= Prathamesh Shivgan
+import java.util.Scanner;
+import anudip.Bankcustomer;
+class Examplepackage
+{
+
+public static void main(String[] args)
+{
+Bankcustomer b=new Bankcustomer();
+b.getdetail(1122,"demo",1000);
+b.show();
+
+}
+}
+ 
